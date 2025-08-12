95306398fb7f1111c3f96ce9b487e412ba7750a7
Nivas Reddy <srinivasreddysagi@gmail.com>
2025-08-13 00:15:00 +0530

    Resuable stylings

---
+++ constants/styles.ts
@@ -0,0 +1,31 @@
+
+// Resuable styles
+
+// Transaction record tile
+export const transactionTileStyles = {
+    container: {
+        borderRadius: 10,
+        marginVertical: 10,
+        padding: 10,
+    },
+    innerContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    note: {
+        color: '#666',
+        fontSize: 12,
+        marginTop: 5,
+    },
+    amount: {
+        color: '#000',
+        fontSize: 16,
+        fontWeight: 'bold',
+    },
+    dateTime: {
+        color: '#999',
+        fontSize: 12,
+        marginTop: 5,
+    }
+}
