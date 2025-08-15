db553e1b897726617c58c1bdc737756d6128f27f
Nivas Reddy <srinivasreddysagi@gmail.com>
2025-08-16 01:18:57 +0530

    Added more common styles

--- constants/styles.ts
+++ constants/styles.ts
@@ -1,8 +1,6 @@
+import { StyleSheet, StatusBar } from 'react-native';
 
-// Resuable styles
-
-// Transaction record tile
-export const transactionTileStyles = {
+export const transactionTileStyles = StyleSheet.create({
     container: {
         borderRadius: 10,
         marginVertical: 10,
@@ -28,4 +26,25 @@ export const transactionTileStyles = {
         fontSize: 12,
         marginTop: 5,
     }
-}
+})
+
+export const transactionSectionStyles = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginTop: StatusBar.currentHeight || 0,
+        alignItems: 'center',
+    },
+    container1: {
+        flex: 1,
+        marginTop: StatusBar.currentHeight || 0,
+    },
+    item: {
+        backgroundColor: '#93DA97',
+        padding: 8,
+        marginVertical: 4,
+        marginHorizontal: 8,
+    },
+    title: {
+        fontSize: 18,
+    },
+});
