3c3465f79f3dc48ffca6020ea852f0c99b9d3547
Nivas Reddy <srinivasreddysagi@gmail.com>
2025-08-17 00:22:24 +0530

    Separate TransactionTile component

---
+++ components/transactions-section/TransactionTile.tsx
@@ -0,0 +1,32 @@
+import { Text, View } from '@/components/Themed';
+import { transactionTileStyles } from '@/constants/styles';
+import { formatCurrency, formatDate } from '@/utils/utils';
+
+export type ITransactionTileProps = {
+    amount: number;
+    trxType?: string;
+    date: Date;
+    source?: string;
+    category?: string[];
+    note: string;
+};
+
+export const TransactionTile = ({
+    amount,
+    date,
+    note,
+}: ITransactionTileProps) => (
+    <View style={transactionTileStyles.container}>
+        <View style={transactionTileStyles.innerContainer}>
+            <Text style={transactionTileStyles.note}>{note}</Text>
+            <Text style={transactionTileStyles.amount}>
+                {formatCurrency(amount)}
+            </Text>
+        </View>
+        <View style={transactionTileStyles.innerContainer}>
+            <Text style={transactionTileStyles.dateTime}>
+                {formatDate(date)}
+            </Text>
+        </View>
+    </View>
+);
