694dca7968cf4bd38be0344fd1d0fd788eb073c3
Nivas Reddy <srinivasreddysagi@gmail.com>
2025-08-19 00:00:22 +0530

    Dropdown input field modifications

--- components/dropdown-field/DropdownField.tsx
+++ components/dropdown-field/DropdownField.tsx
@@ -1,29 +1,18 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import { Dropdown } from 'react-native-paper-dropdown';
 import { Provider as PaperProvider } from 'react-native-paper';
 
-interface IDropdownItem {
-    label: string;
-    value: string;
-}
-
-interface IDropdownPicker {
-    label: string;
-    placeholder: string;
-    value: string;
-    onSelect: React.Dispatch<React.SetStateAction<string>>;
-    options: IDropdownItem[];
-}
+interface IDropdownPicker extends React.ComponentProps<typeof Dropdown> {}
 
 export const DropdownField = (props: IDropdownPicker) => {
     return (
         <PaperProvider>
-            <View style={{ backgroundColor: 'orange', height: 10 }}>
+            <View style={styles.container}>
                 <Dropdown
                     {...props}
                     mode="outlined"
-                    statusBarHeight={-140}
+                    statusBarHeight={-110}
                     hideMenuHeader={true}
                 />
             </View>
@@ -32,3 +21,11 @@ export const DropdownField = (props: IDropdownPicker) => {
 };
 
 export default DropdownField;
+
+const styles = StyleSheet.create({
+    container: {
+        width: 280,
+        maxHeight: 40,
+        margin: 16,
+    },
+});
