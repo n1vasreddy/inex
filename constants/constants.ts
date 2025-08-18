5925b44a091b3506120443516589993836b1db5f
Nivas Reddy <srinivasreddysagi@gmail.com>
2025-08-19 00:00:12 +0530

    Labels from config

---
+++ constants/constants.ts
@@ -0,0 +1,15 @@
+export const labels = {
+    amount: 'Amount',
+    transactionType: 'Transaction Type',
+    date: 'Date',
+    source: 'Source',
+    category: 'Category',
+    note: 'Note',
+};
+
+export const options = {
+    transactionType: [
+        { label: 'Debit', value: 'debit' },
+        { label: 'Credit', value: 'credit' },
+    ],
+};
