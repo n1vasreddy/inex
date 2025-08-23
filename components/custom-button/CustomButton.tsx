dd6e0529fbcd7624bfd10e336b5b40bada2365ca
Nivas Reddy <srinivasreddysagi@gmail.com>
2025-08-24 00:21:18 +0530

    Interface updates to custom button and text input components

--- components/custom-button/CustomButton.tsx
+++ components/custom-button/CustomButton.tsx
@@ -1,7 +1,10 @@
 import * as React from 'react';
+import { StyleProp } from 'react-native';
 import { Button } from 'react-native-paper';
 
-interface ICustomButtonProps extends React.ComponentProps<typeof Button> {}
+interface ICustomButtonProps extends React.ComponentProps<typeof Button> {
+    style?: StyleProp<any>;
+}
 
 const CustomButton = (props: ICustomButtonProps) => (
     <Button {...props} mode="contained">
--- components/text-input-field/TextInputField.tsx
+++ components/text-input-field/TextInputField.tsx
@@ -1,7 +1,10 @@
 import * as React from 'react';
+import { StyleProp } from 'react-native';
 import { TextInput } from 'react-native-paper';
 
-interface ITextInputField extends React.ComponentProps<typeof TextInput> {}
+interface ITextInputField extends React.ComponentProps<typeof TextInput> {
+    style?: StyleProp<any>;
+}
 
 const TextInputField = (props: ITextInputField) => {
     return <TextInput {...props} mode="outlined" />;
