c9e3694e025c7ff8dc85bc71191dd7c5f934cd11
Nivas Reddy <srinivasreddysagi@gmail.com>
2025-08-29 11:15:56 +0530

    Clean up trx section and retrieve actual trx data from store

--- components/transactions-section/TransactionsSection.tsx
+++ components/transactions-section/TransactionsSection.tsx
@@ -1,48 +1,25 @@
-import { FlatList, Pressable } from 'react-native';
-// import { Link } from 'expo-router';
-// import { View } from '@/components/Themed';
+import { FlatList } from 'react-native';
 import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
-// import Ionicons from '@expo/vector-icons/Ionicons';
-// import colors from '@/constants/colors';
-import { useColorScheme } from '@/components/useColorScheme';
 import { TransactionTile, ITransactionTileProps } from './TransactionTile';
-import { transactionTilesMockData } from '@/constants/mockData';
 import { transactionSectionStyles } from '@/constants/styles';
+import { useSelector } from 'react-redux';
 
 export default function TransactionsSection() {
-    // const colorScheme = useColorScheme();
+    const transactions = useSelector((state: any) => state.transactions.data);
 
     return (
         <>
             <SafeAreaProvider>
                 <SafeAreaView style={transactionSectionStyles.container}>
                     <FlatList
-                        data={transactionTilesMockData}
+                        data={transactions}
                         renderItem={(props: {
                             item: ITransactionTileProps;
                         }) => <TransactionTile {...props.item} />}
-                        keyExtractor={(item: any) => item.id}
+                        keyExtractor={(item: any) => item.amount}
                     />
                 </SafeAreaView>
             </SafeAreaProvider>
-
-            {/* <View style={transactionSectionStyles.container}>
-                <Link href="/addTransaction" asChild>
-                    <Pressable>
-                        {({ pressed = false }: { pressed?: boolean }) => (
-                            <Ionicons
-                                name="add-circle"
-                                size={45}
-                                color={colors[colorScheme ?? 'light'].text}
-                                style={{
-                                    marginRight: 15,
-                                    opacity: pressed ? 0.5 : 1,
-                                }}
-                            />
-                        )}
-                    </Pressable>
-                </Link>
-            </View> */}
         </>
     );
 }
