3755a5ae49961b39f5fe1216cdf1a0259eb33b96
Nivas Reddy <srinivasreddysagi@gmail.com>
2025-08-29 08:32:52 +0530

    Update mock data for testing

--- constants/mockData.ts
+++ constants/mockData.ts
@@ -1,35 +1,12 @@
-
 // Mock data - to be removed later
 export const transactionTilesMockData = [
     {
         id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-        title: 'First Item',
         amount: 100,
         trxType: 'Debit',
-        date: new Date(),
-        source: 'HDFC Bank',
-        category: ['Food', 'Grocery'],
-        note: 'Some note',
-    },
-    {
-        id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-        title: 'Second Item',
-        amount: 20,
-        trxType: 'Debit',
-        date: new Date(),
-        source: 'HDFC Bank',
-        category: ['Food', 'Grocery'],
-        note: 'Some note',
-    },
-    {
-        id: '58694a0f-3da1-471f-bd96-145571e29d72',
-        title: 'Third Item',
-        amount: 5500,
-        trxType: 'Debit',
-        date: new Date(),
+        date: new Date().toJSON(),
         source: 'HDFC Bank',
         category: ['Food', 'Grocery'],
         note: 'Some note',
     },
 ];
-
