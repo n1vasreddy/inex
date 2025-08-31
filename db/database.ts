61e1eea1c27940896afec22cfbde446d4dc88b88
Nivas Reddy <srinivasreddysagi@gmail.com>
2025-09-01 01:46:08 +0530

    Function to create transactions table if not exist

--- db/database.ts
+++ db/database.ts
@@ -13,3 +13,23 @@ export async function openDatabase() {
         return db!;
     }
 }
+
+export async function setupDatabase() {
+    try {
+        const db = await openDatabase();
+        await db.execAsync(`
+    CREATE TABLE IF NOT EXISTS transactions (
+        id TEXT PRIMARY KEY NOT NULL,
+        amount REAL NOT NULL DEFAULT 0,
+        trxType TEXT NOT NULL,
+        date string NOT NULL,
+        paymentMethod TEXT NOT NULL,
+        category TEXT,
+        note TEXT
+    );
+    `);
+    } catch (error) {
+        console.error('Error setting up database:', error);
+        return [];
+    }
+}
