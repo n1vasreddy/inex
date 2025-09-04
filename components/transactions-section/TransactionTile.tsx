d8e795572270bfc877820e0345861133180c5c83
Nivas Reddy <srinivasreddysagi@gmail.com>
2025-09-05 00:44:22 +0530

    Simplified transaction tile

--- components/transactions-section/TransactionTile.tsx
+++ components/transactions-section/TransactionTile.tsx
@@ -1,14 +1,10 @@
 import { Text, View } from '@/components/Themed';
 import { transactionTileStyles } from '@/constants/styles';
 import { formatCurrency, formatDate } from '@/utils/utils';
-import { TransactionType } from '@/constants/constants';
 
 export type ITransactionTileProps = {
     amount: number;
-    trxType?: TransactionType;
     date: string;
-    paymentMethod?: string;
-    category?: string[];
     note: string;
 };
 
