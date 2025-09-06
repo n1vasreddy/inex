9973bbe4db07d1b3b90739e2cb0949e94bef3dcf
Nivas Reddy <srinivasreddysagi@gmail.com>
2025-09-06 16:57:19 +0530

    Configure middleware for redux store

--- store/store.ts
+++ store/store.ts
@@ -7,6 +7,10 @@ const rootReducer = {
 
 const store = configureStore({
     reducer: rootReducer,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
 });
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
 
 export default store;
