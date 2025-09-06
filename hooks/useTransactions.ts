12117fbcea55d5b9bafc67dabc405291f78abc1a
Nivas Reddy <srinivasreddysagi@gmail.com>
2025-09-06 18:41:46 +0530

    Remove unnecessary refresh from transactions hook

--- hooks/useTransactions.ts
+++ hooks/useTransactions.ts
@@ -1,25 +1,9 @@
-import { useState, useEffect } from 'react';
-import { getTransactions, addTransaction } from '@/db/database';
+import { addTransaction } from '@/db/database';
 
 export default function useTransactions() {
-    const [transactions, setTransactions] = useState<any>([]);
-
-    const refresh = async () => {
-        const trxs = await getTransactions();
-        console.log('TRXs from DB >>> ', trxs);
-        setTransactions(trxs);
-    };
-
-    useEffect(() => {
-        refresh();
-    }, []);
-
     return {
-        transactions,
         add: async (trx: any) => {
             await addTransaction(trx);
-            refresh();
         },
-        refresh,
     };
 }
