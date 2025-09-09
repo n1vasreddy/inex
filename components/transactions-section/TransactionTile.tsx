6a85b4be5a997fea9e966c44ed0039eece5253b7
Nivas Reddy <srinivasreddysagi@gmail.com>
2025-09-09 16:36:14 +0530

    Action buttons template for TRX tile

--- components/transactions-section/TransactionTile.tsx
+++ components/transactions-section/TransactionTile.tsx
@@ -1,7 +1,9 @@
+import { useState } from 'react';
+import { StyleSheet, Pressable } from 'react-native';
 import { Text, View } from '@/components/Themed';
-import { transactionTileStyles } from '@/constants/styles';
 import { formatCurrency, formatDate } from '@/utils/utils';
 import { ITransactionInfo } from '@/store/transactions';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 export interface ITransactionTileProps extends ITransactionInfo {
     color: string;
@@ -12,20 +14,122 @@ export const TransactionTile = ({
     date,
     note,
     color,
-}: ITransactionTileProps) => (
-    <View
-        style={[transactionTileStyles.container, { borderRightColor: color }]}
-    >
-        <View style={transactionTileStyles.innerContainer}>
-            <Text style={transactionTileStyles.note}>{note}</Text>
-            <Text style={transactionTileStyles.amount}>
-                {formatCurrency(amount)}
-            </Text>
-        </View>
-        <View style={transactionTileStyles.innerContainer}>
-            <Text style={transactionTileStyles.dateTime}>
-                {formatDate(new Date(date))}
-            </Text>
-        </View>
-    </View>
-);
+}: ITransactionTileProps) => {
+    const [zIndex, setZIndex] = useState(-1);
+
+    const handleLongPress = () => {
+        setZIndex(1);
+    };
+
+    const handleDelete = () => {
+        setZIndex(-1);
+    };
+
+    const handleEdit = () => {
+        setZIndex(-1);
+    };
+
+    const handleCancel = () => {
+        setZIndex(-1);
+    };
+
+    return (
+        <Pressable onLongPress={handleLongPress}>
+            <View
+                style={[
+                    transactionTileStyles.container,
+                    { borderRightColor: color },
+                ]}
+            >
+                <View style={transactionTileStyles.innerContainer}>
+                    <Text style={transactionTileStyles.note}>{note}</Text>
+                    <Text style={transactionTileStyles.amount}>
+                        {formatCurrency(amount)}
+                    </Text>
+                </View>
+                <View style={transactionTileStyles.innerContainer}>
+                    <Text style={transactionTileStyles.dateTime}>
+                        {formatDate(new Date(date))}
+                    </Text>
+                </View>
+                <View
+                    style={[transactionTileStyles.actionsContainer, { zIndex }]}
+                >
+                    <Pressable
+                        onPress={handleDelete}
+                        style={transactionTileStyles.deleteButton}
+                    >
+                        <MaterialCommunityIcons
+                            name="trash-can"
+                            size={28}
+                            color="#f51720"
+                        />
+                    </Pressable>
+                    <Pressable
+                        onPress={handleEdit}
+                        style={transactionTileStyles.editButton}
+                    >
+                        <MaterialCommunityIcons
+                            name="square-edit-outline"
+                            size={28}
+                            color="#f51720"
+                        />
+                    </Pressable>
+                    <Pressable
+                        onPress={handleCancel}
+                        style={transactionTileStyles.cancelButton}
+                    >
+                        <MaterialCommunityIcons
+                            name="window-close"
+                            size={28}
+                            color="#f51720"
+                        />
+                    </Pressable>
+                </View>
+            </View>
+        </Pressable>
+    );
+};
+
+const transactionTileStyles = StyleSheet.create({
+    container: {
+        borderRadius: 5,
+        marginVertical: 10,
+        padding: 10,
+        borderRightWidth: 2,
+    },
+    innerContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    note: {
+        color: '#666',
+        fontSize: 12,
+    },
+    amount: {
+        color: '#000',
+        fontSize: 16,
+        fontWeight: 'bold',
+    },
+    dateTime: {
+        color: '#999',
+        fontSize: 12,
+        marginTop: 5,
+    },
+    actionsContainer: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+    },
+    deleteButton: {},
+    editButton: {},
+    cancelButton: {},
+});
+
+// note-edit square-edit-outline
