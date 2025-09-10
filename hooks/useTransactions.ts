3bc7ee7476b04aa17b203f5d7d79abd78e5943aa
Nivas Reddy <srinivasreddysagi@gmail.com>
2025-09-10 15:04:12 +0530

    Return update call from useTransaction hook

--- hooks/useTransactions.ts
+++ hooks/useTransactions.ts
@@ -1,9 +1,12 @@
-import { addTransaction } from '@/db/database';
+import { addTransaction, updateTransaction } from '@/db/database';
 
 export default function useTransactions() {
     return {
         add: async (trx: any) => {
             await addTransaction(trx);
+        },
+        update: async (trx: any) => {
+            await updateTransaction(trx);
         },
     };
 }
