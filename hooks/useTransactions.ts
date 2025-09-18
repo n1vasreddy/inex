cebedcc2ba2cfcb5aee9de6788c545a1d3c60132
Nivas Reddy <srinivasreddysagi@gmail.com>
2025-09-18 12:18:54 +0530

    Add export to excel functionality in useTrx hook

--- hooks/useTransactions.ts
+++ hooks/useTransactions.ts
@@ -4,13 +4,66 @@ import {
     addTransaction,
     updateTransaction,
     deleteTransaction,
+    getTransactions,
 } from '@/db/queries';
+import { ITransactionInfo } from '@/store/transactions';
+import { openDatabase } from '@/db/database';
+import XLSX from 'xlsx';
+import * as FileSystem from 'expo-file-system';
+import * as Sharing from 'expo-sharing';
+import { StorageAccessFramework } from 'expo-file-system';
 
 export default function useTransactions() {
     const dispatch: AppDispatch = useAppDispatch();
 
     const refresh = async () => {
         dispatch(loadTransactions());
+    };
+
+    const exportToExcel = async () => {
+        await openDatabase();
+        try {
+            const allTransactions: ITransactionInfo[] = await getTransactions();
+
+            const ws = XLSX.utils.json_to_sheet(allTransactions);
+            const wb = XLSX.utils.book_new();
+            XLSX.utils.book_append_sheet(wb, ws, 'Transactions');
+
+            const wbout = XLSX.write(wb, { type: 'base64', bookType: 'xlsx' });
+
+            const fileName = 'transactions.xlsx';
+            const localUri = FileSystem.cacheDirectory + fileName;
+
+            await FileSystem.writeAsStringAsync(localUri, wbout, {
+                encoding: FileSystem.EncodingType.Base64,
+            });
+
+            const permissions =
+                await StorageAccessFramework.requestDirectoryPermissionsAsync();
+            if (!permissions.granted) {
+                alert('You need to allow directory access.');
+                return;
+            }
+
+            const fileUri = await StorageAccessFramework.createFileAsync(
+                permissions.directoryUri,
+                fileName,
+                'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+            );
+            if (!fileUri)
+                throw new Error('Could not create file in Downloads directory');
+
+            const fileContent = await FileSystem.readAsStringAsync(localUri, {
+                encoding: FileSystem.EncodingType.Base64,
+            });
+            await FileSystem.writeAsStringAsync(fileUri, fileContent, {
+                encoding: FileSystem.EncodingType.Base64,
+            });
+
+            alert('Excel file exported to selected folder!');
+        } catch (error) {
+            console.error('Error exporting transactions:', error);
+        }
     };
 
     return {
@@ -23,6 +76,7 @@ export default function useTransactions() {
         remove: async (id: string) => {
             await deleteTransaction(id);
         },
+        exportToExcel,
         refresh,
     };
 }
