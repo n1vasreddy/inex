49bf7c4f5cbe169268aab367a5d89f960665a80e
Nivas Reddy <srinivasreddysagi@gmail.com>
2025-09-27 10:25:52 +0530

    Add tagsSlice in store

---
+++ store/tags.ts
@@ -0,0 +1,45 @@
+import { createSlice, createAsyncThunk, PayloadAction } from '@reduxjs/toolkit';
+// import { getTransactions } from '../db/queries';
+
+export interface ITag {
+    id: string;
+    tagName: string;
+    tagEmoji: string;
+}
+
+interface ITags {
+    data: ITag[];
+}
+
+interface ITagEntry {
+    type: string;
+    payload: ITag;
+}
+
+export const tagsSlice = createSlice({
+    name: 'tags',
+    initialState: { data: [] } as ITags,
+    reducers: {
+        tagEntry: (state: ITags, action: ITagEntry) => {
+            state.data = [...state.data, action.payload];
+        },
+    },
+    // extraReducers: (builder) => {
+    //     builder.addCase(
+    //         loadTags.fulfilled,
+    //         (state, action: PayloadAction<IActionTransactionEntry[]>) => {
+    //             state.data = action.payload;
+    //         },
+    //     );
+    // },
+});
+
+// export const loadTags = createAsyncThunk(
+//     'tags/loadTags',
+//     async (): Promise<ITag[]> => {
+//         return await getTransactions();
+//     },
+// );
+
+export const { tagEntry } = tagsSlice.actions;
+export default tagsSlice.reducer;
