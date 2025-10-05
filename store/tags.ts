fcbf5e260e100cf988356702355ced5126ec95cb
Nivas Reddy <srinivasreddysagi@gmail.com>
2025-10-05 13:27:14 +0530

    Thunk action to fetch all tags

--- store/tags.ts
+++ store/tags.ts
@@ -1,6 +1,5 @@
 import { createSlice, createAsyncThunk, PayloadAction } from '@reduxjs/toolkit';
-// import { getTransactions } from '../db/queries';
-
+import { getTags } from '@/db/queries';
 export interface ITag {
     id: string;
     tagName: string;
@@ -16,37 +15,30 @@ interface ITagEntry {
     payload: ITag;
 }
 
-const mockTags: ITag[] = [
-    { id: '1', tagEmoji: '🍔', tagName: 'Food' },
-    { id: '2', tagEmoji: '✈️', tagName: 'Travel' },
-    { id: '3', tagEmoji: '🏠', tagName: 'Rent' },
-    { id: '4', tagEmoji: '🎁', tagName: 'Gifts' },
-];
-
 export const tagsSlice = createSlice({
     name: 'tags',
-    initialState: { data: mockTags } as ITags,
+    initialState: { data: [] } as ITags,
     reducers: {
         tagEntry: (state: ITags, action: ITagEntry) => {
             state.data = [...state.data, action.payload];
         },
     },
-    // extraReducers: (builder) => {
-    //     builder.addCase(
-    //         loadTags.fulfilled,
-    //         (state, action: PayloadAction<IActionTransactionEntry[]>) => {
-    //             state.data = action.payload;
-    //         },
-    //     );
-    // },
+    extraReducers: (builder) => {
+        builder.addCase(
+            loadTags.fulfilled,
+            (state, action: PayloadAction<ITag[]>) => {
+                state.data = action.payload;
+            },
+        );
+    },
 });
 
-// export const loadTags = createAsyncThunk(
-//     'tags/loadTags',
-//     async (): Promise<ITag[]> => {
-//         return await getTransactions();
-//     },
-// );
+export const loadTags = createAsyncThunk(
+    'tags/loadTags',
+    async (): Promise<ITag[]> => {
+        return await getTags();
+    },
+);
 
 export const { tagEntry } = tagsSlice.actions;
 export default tagsSlice.reducer;
