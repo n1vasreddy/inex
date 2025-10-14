ac4c37116dde951a8baca569024aa2c6cbbd51a3
Nivas Reddy <srinivasreddysagi@gmail.com>
2025-10-14 10:40:00 +0530

    StyledText component

---
+++ components/styled-text/StyledText.tsx
@@ -0,0 +1,7 @@
+import { Text, TextProps } from '../Themed';
+
+export function StyledText(props: TextProps) {
+    return (
+        <Text {...props} style={[props.style, { fontFamily: 'SpaceMono' }]} />
+    );
+}
