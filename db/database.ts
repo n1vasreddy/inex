0769c6304d3aadb93b8ea98b67e59028519e2efe
Nivas Reddy <srinivasreddysagi@gmail.com>
2025-10-17 14:49:00 +0530

    Removed unnecessary try catch block for db

--- db/database.ts
+++ db/database.ts
@@ -3,15 +3,10 @@ import * as SQLite from 'expo-sqlite';
 let db: SQLite.SQLiteDatabase | null = null;
 
 export async function openDatabase() {
-    try {
-        if (!db) {
-            db = await SQLite.openDatabaseAsync('inex.db');
-        }
-    } catch (error) {
-        console.error('Error opening database:', error);
-    } finally {
-        return db!;
+    if (!db) {
+        db = await SQLite.openDatabaseAsync('inex.db');
     }
+    return db;
 }
 
 export async function setupDatabase(schema: string) {
