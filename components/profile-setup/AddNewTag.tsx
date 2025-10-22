4cd74395893360bbf80165272a9a826442684095
Nivas Reddy <srinivasreddysagi@gmail.com>
2025-10-23 01:07:00 +0530

    Themed AddNewTag page

--- components/profile-setup/AddNewTag.tsx
+++ components/profile-setup/AddNewTag.tsx
@@ -1,10 +1,15 @@
 import React, { useState } from 'react';
-import { View, StyleSheet } from 'react-native';
-import { TextInput, Button } from 'react-native-paper';
+import { View, StyleSheet, useColorScheme } from 'react-native';
+import TextInput from '@/components/text-input-field/TextInputField';
+import Button from '@/components/custom-button/CustomButton';
 import { useNavigation } from 'expo-router';
 import useTags from '@/hooks/useTags';
+import colors from '@/constants/Colors';
+import { labels } from '@/constants/constants';
+import { PaperProvider } from 'react-native-paper';
 
 const AddNewTag = () => {
+    const colorScheme = useColorScheme();
     const navigation = useNavigation();
     const { addTag } = useTags();
     const [tagName, setTagName] = useState('');
@@ -23,21 +28,36 @@ const AddNewTag = () => {
     };
 
     return (
-        <View style={addNewTagStyles.container}>
-            <TextInput
-                label="Tag Name"
-                value={tagName}
-                onChangeText={(text) => setTagName(text)}
-            />
-            <TextInput
-                label="Tag Emoji"
-                value={tagEmoji}
-                onChangeText={(text) => setTagEmoji(text)}
-            />
-            <Button mode="contained" onPress={handleAddTag}>
-                Add Tag
-            </Button>
-        </View>
+        <PaperProvider>
+            <View
+                style={[
+                    {
+                        backgroundColor:
+                            colors[colorScheme ?? 'light'].background,
+                    },
+                    addNewTagStyles.container,
+                ]}
+            >
+                <TextInput
+                    label={labels.tagName}
+                    value={tagName}
+                    onChangeText={(text) => setTagName(text)}
+                />
+                <TextInput
+                    label={labels.tagEmoji}
+                    value={tagEmoji}
+                    onChangeText={(text) => setTagEmoji(text)}
+                />
+                <Button
+                    mode="contained"
+                    onPress={handleAddTag}
+                    labelStyle={{ color: colors.babyBlue }}
+                    style={{ backgroundColor: colors.fuchsia, marginTop: 16 }}
+                >
+                    {labels.addTag}
+                </Button>
+            </View>
+        </PaperProvider>
     );
 };
 
@@ -49,7 +69,6 @@ const addNewTagStyles = StyleSheet.create({
         padding: 16,
         flexDirection: 'column',
         rowGap: 10,
-        backgroundColor: '#fff',
         justifyContent: 'center',
     },
 });
