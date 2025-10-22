1d25a006988454ac94e4319123cbe2df30c9fc1e
Nivas Reddy <srinivasreddysagi@gmail.com>
2025-10-23 01:05:00 +0530

    Remove unnecessary props in CustomButton component

--- components/custom-button/CustomButton.tsx
+++ components/custom-button/CustomButton.tsx
@@ -1,10 +1,7 @@
 import * as React from 'react';
-import { StyleProp } from 'react-native';
 import { Button } from 'react-native-paper';
 
-interface ICustomButtonProps extends React.ComponentProps<typeof Button> {
-    style?: StyleProp<any>;
-}
+interface ICustomButtonProps extends React.ComponentProps<typeof Button> {}
 
 const CustomButton = (props: ICustomButtonProps) => (
     <Button {...props} mode="contained">
