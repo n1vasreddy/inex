98eeae57713a21157a22e39517e4102ce07fcd1f
Nivas Reddy <srinivasreddysagi@gmail.com>
2025-11-08 10:05:00 +0530

    Handle submit/update functionality

--- components/profile-setup/AddAccount.tsx
+++ components/profile-setup/AddAccount.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, StyleSheet, useColorScheme } from 'react-native';
 import TextInput from '@/components/text-input-field/TextInputField';
 import Button from '@/components/custom-button/CustomButton';
@@ -6,16 +6,42 @@ import { PaperProvider } from 'react-native-paper';
 import RadioButtonInput from '@/components/radio-group-input/RadioButtonInput';
 import { labels, options } from '@/constants/constants';
 import colors from '@/constants/Colors';
+import { IAccountInfo } from '@/store/accounts';
+import { useNavigation } from 'expo-router';
+import useAccounts from '@/hooks/useAccounts';
 
-const AddAccount = () => {
+const AddAccount = (props: IAccountInfo) => {
     const colorScheme = useColorScheme() ?? 'light';
+    const navigation = useNavigation();
+    const [isUpdate, setIsUpdate] = useState(false);
     const [label, setLabel] = useState('');
     const [value, setValue] = useState('');
     const [type, setType] = useState('');
     const [balance, setBalance] = useState('');
+    const { addAccount, updateAccountInfo } = useAccounts();
 
-    const handleSubmit = () => {
-        // Handle form submission logic here
+    useEffect(() => {
+        if (props?.value) {
+            setIsUpdate(true);
+            props?.balance && setBalance(props.balance.toString());
+            props?.type && setType(props.type);
+            props?.label && setLabel(props.label);
+        }
+    }, [props?.value]);
+
+    const handleSubmit = async () => {
+        const payload = {
+            label,
+            value,
+            type,
+            balance: Number(balance),
+        };
+        if (isUpdate) {
+            await updateAccountInfo(payload);
+        } else {
+            await addAccount(payload);
+        }
+        navigation.goBack();
     };
 
     return (
@@ -36,10 +62,12 @@ const AddAccount = () => {
                 <TextInput
                     label={labels.value}
                     value={value}
+                    disabled={isUpdate}
                     onChangeText={(text) => setValue(text)}
                 />
                 <RadioButtonInput
                     options={options.accountTypeOptions}
+                    disabled={isUpdate}
                     onValueChange={(value) => setType(value)}
                     value={type}
                     label={labels.accountType}
