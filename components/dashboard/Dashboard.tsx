572d7b34ef47adf70469c49a7eebf8888cdb972c
Nivas Reddy <srinivasreddysagi@gmail.com>
2025-11-15 02:48:00 +0530

    Merge branch 'feature/dashboard'

--- components/dashboard/Dashboard.tsx
+++ components/dashboard/Dashboard.tsx
@@ -1,7 +1,24 @@
+import colors from '@/constants/Colors';
 import React from 'react';
+import { View, StyleSheet, useColorScheme } from 'react-native';
 
 const Dashboard = () => {
-    return <></>;
+    const colorScheme = useColorScheme();
+
+    return (
+        <View
+            style={[
+                {
+                    backgroundColor: colors[colorScheme ?? 'light'].background,
+                },
+                styles.container,
+            ]}
+        ></View>
+    );
 };
 
 export default Dashboard;
+
+const styles = StyleSheet.create({
+    container: { flex: 1 },
+});
